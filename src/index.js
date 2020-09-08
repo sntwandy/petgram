16e56a69d20ee7af6590e55d65f21de7ef516903
sntwandy <sntwandy@gmail.com>
2020-09-08 01:08:17 -0400

    Configuring babel and presets

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("We starting!");
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Hey, hi!</h1>, document.querySelector("#App"));
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,23 @@ module.exports = {
   output: {
     filename: "app.bundle.js",
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
 };
