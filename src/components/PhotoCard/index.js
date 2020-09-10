a745b4fc0efaae241f81bf909fc7bcf512498ccd
sntwandy <sntwandy@gmail.com>
2020-09-10 11:01:47 -0400

    Using Intersection Observer to bring support to all browsers using window.IntersectionObserver using a Polyfill

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -12,16 +12,24 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, setShow] = useState(false);
 
   useEffect(() => {
-    // console.log(ref.current);
-    const observer = new window.IntersectionObserver((entries) => {
-      const { isIntersecting } = entries[0];
+    // Intersection observer, support to all browsers
+    Promise.resolve(
+      // If the browser has the type IntersectionObserver return window, if not, import
+      typeof window.IntersectionObserver !== undefined
+        ? window.IntersectionObserver
+        : import("intersection-observer")
+    ).then(() => {
+      // Return the promise with window or import to resolve
+      const observer = new window.IntersectionObserver((entries) => {
+        const { isIntersecting } = entries[0];
 
-      if (isIntersecting) {
-        setShow(true);
-        observer.disconnect();
-      }
+        if (isIntersecting) {
+          setShow(true);
+          observer.disconnect();
+        }
+      });
+      observer.observe(ref.current);
     });
-    observer.observe(ref.current);
   }, [ref]);
 
   return (
