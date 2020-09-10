07d062c10d572ccfdedbd418d3cd94a3947bf18d
sntwandy <sntwandy@gmail.com>
2020-09-10 10:32:52 -0400

    Using Lazy load with custom hooks

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MdFavoriteBorder } from "react-icons/md";
 
 // Styles
-import { ImgWrapper, Img, Button } from "./styles";
+import { ImgWrapper, Img, Button, Article } from "./styles";
 
 const DEFAULT_IMAGE =
   "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const ref = useRef(null);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    // console.log(ref.current);
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting } = entries[0];
+
+      if (isIntersecting) {
+        setShow(true);
+        observer.disconnect();
+      }
+    });
+    observer.observe(ref.current);
+  }, [ref]);
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} alt="img detail" />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size="32px" />
-        {likes}Likes!
-      </Button>
-    </article>
+    <Article ref={ref}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} alt="img detail" />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size="32px" />
+            {likes}Likes!
+          </Button>
+        </>
+      )}
+    </Article>
   );
 };
