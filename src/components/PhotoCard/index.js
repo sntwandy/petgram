f681e1559b6816da5ce3ead152568eb8bde59d88
sntwandy <sntwandy@gmail.com>
2020-09-26 02:27:09 -0400

    Optimization of the code, graphql, queries names and renderProps

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -11,7 +11,7 @@ import { useNearScreeen } from "../../hooks/useNearScreen";
 const DEFAULT_IMAGE =
   "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, likes = 0, src }) => {
   const [show, ref] = useNearScreeen();
   // get the like from localStorage [hooks]
   const key = `${id}.like`;
--- src/container/ListOfPhotoCardsWithQuery.js
+++ src/container/ListOfPhotoCardsWithQuery.js
@@ -5,7 +5,7 @@ import { Query } from "react-apollo";
 // Components
 import { ListOfPhotoCardsComponent } from "../components/ListOfPhotoCards";
 
-const query = gql`
+const GET_SINGLE_PHOTO = gql`
   query getPhotos($categoryId: ID) {
     photos(categoryId: $categoryId) {
       id
@@ -18,11 +18,15 @@ const query = gql`
   }
 `;
 
+const renderProp = ({ loading, error, data = { photos: [] } }) => {
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+  // console.log(data);
+  return <ListOfPhotoCardsComponent data={data} />;
+};
+
 export const ListOfPhotoCardsWithQuery = ({ categoryId }) => (
-  <Query query={query} variables={{ categoryId }}>
-    {({ loading, error, data = { photos: [] } }) => {
-      // console.log(data);
-      return <ListOfPhotoCardsComponent data={data} />;
-    }}
+  <Query query={GET_SINGLE_PHOTO} variables={{ categoryId }}>
+    {renderProp}
   </Query>
 );
