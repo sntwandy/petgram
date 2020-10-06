4539a53b29ce13fed0e8674ca5864c2958bd06db
sntwandy <sntwandy@gmail.com>
2020-10-06 03:41:17 -0400

    Protected routes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
 import { Logo } from "./components/Logo";
 import { NavBar } from "./components/NavBar";
+import { Favs } from "./pages/Favs";
+import { NotRegisteredUser } from "./pages/NotRegisteredUser";
+import { User } from "./pages/User";
 
 // Global Styles
 import GlobalStyles from "./styles/GlobalStyles";
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false });
+};
 
 export const App = () => {
   return (
@@ -20,6 +27,25 @@ export const App = () => {
         <Home path="/pet/:id" />
         <Detail path="/detail/:detailId" />
       </Router>
+      <UserLogged>
+        {({ isAuth }) => {
+          if (isAuth) {
+            return (
+              <Router>
+                <Favs path="/favs" />
+                <User path="/user" />
+              </Router>
+            );
+          } else {
+            return (
+              <Router>
+                <NotRegisteredUser path="/user" />
+                <NotRegisteredUser path="/favs" />
+              </Router>
+            );
+          }
+        }}
+      </UserLogged>
       <NavBar />
     </>
   );
